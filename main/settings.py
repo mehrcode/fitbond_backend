52796304ddff3577a6eb2e995b4d71a274fa9b28
mehrcode <mehrnoosh.shoolestani@gmail.com>
2025-05-18 15:06:25 +0330

    fix some bugs and update setings.py

--- main/settings.py
+++ main/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     "corsheaders",
     "account",
     "habit",
+    "main",
 ]
 
 MIDDLEWARE = [
--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
 
+
+def home(request):
+    return HttpResponse("صفحه‌ی اصلی کار می‌کند!")
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("account/", include("account.urls")),
-    path('habit/', include('habit.urls')),
-    path('api/account/', include('account.api.urls')),
+    path("habit/", include("habit.urls")),
+    path("api/account/", include("account.api.urls")),
+    path("", home, name="home"),
 ]
