5db63c819fcb41f014511a0f597f05a97932ab6a
mehrcode <mehrnoosh.shoolestani@gmail.com>
2025-05-25 11:54:15 +0330

    make serializer and view and url for api/account

--- account/api/urls.py
+++ account/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from .views import UserProfileView
 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("profile/", UserProfileView.as_view(), name="user-profile"),
 ]
